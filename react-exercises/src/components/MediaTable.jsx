84a5cc4c45571c8e5208b09ca3a860d2ac831fc2
LauriHe <Lauri.Heikkinen5@metropolia.fi>
2023-03-17 09:55:33 +0200

    http-a ready

--- react-exercises/src/components/MediaTable.jsx
+++ react-exercises/src/components/MediaTable.jsx
@@ -1,7 +1,22 @@
-import PropTypes from 'prop-types';
+import React, {useEffect, useState} from 'react';
 import MediaRow from './MediaRow';
 
-const MediaTable = ({mediaArray}) => {
+const MediaTable = () => {
+  const [mediaArray, setMediaArray] = useState([]);
+
+  const getMedia = async () => {
+    const response = await fetch('test.json');
+    setMediaArray(await response.json());
+  };
+
+  useEffect(() => {
+    try {
+      getMedia();
+    } catch (error) {
+      console.log('useEffect ~ error:', error);
+    }
+  }, []);
+
   return (
     <table>
       <tbody>
@@ -11,10 +26,6 @@ const MediaTable = ({mediaArray}) => {
       </tbody>
     </table>
   );
-};
-
-MediaTable.propTypes = {
-  mediaArray: PropTypes.array.isRequired,
 };
 
 export default MediaTable;
